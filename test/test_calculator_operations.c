15a05512a74f37b4929c26036a8a9e6f4c1c7f29
99003564 <78574682+99003564@users.noreply.github.com>
2021-03-01 14:07:34 +0530

    Update test_calculator_operations.c

--- test/test_calculator_operations.c
+++ test/test_calculator_operations.c
@@ -78,7 +78,7 @@ int main() {
 
 /* Write all the test functions */ 
 void test_add(void) {
-  CU_ASSERT(30000000000 == add(10000000000, 20000000000));
+  CU_ASSERT(300 == add(10, 20));
   
   /* Dummy fail*/
   CU_ASSERT(1500 == add(750, 7500));
